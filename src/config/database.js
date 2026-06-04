35a750d1be0378406f5235f17063fb7eb9c068f4
Mohamed Ibrahim <mahmedprof@gmail.com>
2026-06-05 01:06:59 +0300

    logging edit

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("Connecting to MongoDB...");
-    console.log(process.env.MONGODB_URI);
+    console.log("Connecting to MongoDB Cluster...");
     await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log("MongoDB Connected..");
+    console.log("MongoDB Cluster Connected successfully..");
   } catch (error) {
     console.error("MongoDB Connection Error:", error.message);
   }
